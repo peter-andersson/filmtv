8e69001f1ba9322ca1d1bbc77f0958b4a9c2b95a
Peter Andersson <peter@lpda.se>
2024-04-27 12:14:30 +0200

    OpenTelemetry

--- src/FilmTV/FilmTV/Program.cs
+++ src/FilmTV/FilmTV/Program.cs
@@ -4,8 +4,45 @@ using Microsoft.EntityFrameworkCore;
 using FilmTV.Components;
 using FilmTV.Components.Account;
 using FilmTV.Data;
+using OpenTelemetry.Logs;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Trace;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Logging.AddOpenTelemetry(logging =>
+{
+    logging.IncludeFormattedMessage = true;
+    logging.IncludeScopes = true;
+});
+
+builder.Services.AddOpenTelemetry()
+    .WithMetrics(metrics =>
+    {
+        metrics.AddRuntimeInstrumentation()
+            .AddMeter("Microsoft.AspNetCore.Hosting", "Microsoft.AspNetCore.Server.Kestrel", "System.Net.Http");
+    })
+    .WithTracing(tracing =>
+    {
+        if (builder.Environment.IsDevelopment())
+        {
+            tracing.SetSampler(new AlwaysOnSampler());
+        }
+
+        tracing.SetErrorStatusOnException();
+
+        tracing.AddAspNetCoreInstrumentation()
+            .AddHttpClientInstrumentation()
+            .AddEntityFrameworkCoreInstrumentation();
+    });
+
+var useOtlpExporter = !string.IsNullOrWhiteSpace(builder.Configuration["OTEL_EXPORTER_OTLP_ENDPOINT"]);
+if (useOtlpExporter)
+{
+    builder.Services.Configure<OpenTelemetryLoggerOptions>(logging => logging.AddOtlpExporter());
+    builder.Services.ConfigureOpenTelemetryMeterProvider(metrics => metrics.AddOtlpExporter());
+    builder.Services.ConfigureOpenTelemetryTracerProvider(tracing => tracing.AddOtlpExporter());
+}
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
