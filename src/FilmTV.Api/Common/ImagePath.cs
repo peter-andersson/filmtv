72390515915e406f1a0cb19b9bd7d456ea2f8a57
Peter Andersson <peter@lpda.se>
2024-05-12 11:35:39 +0200

    Changed TvShowPath to SeriesPath

--- src/FilmTV.Api/Common/ImagePath.cs
+++ src/FilmTV.Api/Common/ImagePath.cs
@@ -6,5 +6,5 @@ public static class ImagePath
     
     public static string MoviePath(int id) => $"{Directory}/movie/{id}.jpg";
     
-    public static string TvShowPath(int id) => $"{Directory}/tv/{id}.jpg";
+    public static string SeriesPath(int id) => $"{Directory}/tv/{id}.jpg";
 }
